03803853d1d394cfcb9807103f1ca6f91b0dc883
Alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2022-02-16 14:19:38 +0300

    commiting css files

--- handeling_routes.js
+++ handeling_routes.js
@@ -1,19 +1,29 @@
-var http=require('http');
-var routings=require('./routing');
 
+const http = require('http');
+const fs = require('fs');
 
-var path=require('path');
-var express =require('express');
-var app=express();
- app.use(express.static(path.join(__dirname, 'public')));
+const file = (fileName) => fs.createReadStream(fileName, 'utf-8');
 
+http
+  .createServer((req, res) => {
+    if (req.url.startsWith('/style.css')) {
+      res.setHeader('Content-Type', 'text/css');
+      file('./public/css/style.css').pipe(res);
+    }
 
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'html');
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
-// app.get('/index', function(req, res){
-//     res.render('../views/index.html');
-// });
-http.createServer(routings.handleRequest).listen(7100);
+    if (req.url.startsWith('/admin')) {
+      if (req.url === '/admin?role=admin') {
+        file('./views/home.html').pipe(res);
+      } else {
+        file('./views/login.html').pipe(res);
+      }
+    } else if (req.url === '/') {
+      file('./views/index.html').pipe(res);
+    } else if (req.url === '/recent') {
+      file('./views/recent.html').pipe(res);
+    }
+    else if (req.url === '/login') {
+        file('./views/login.html').pipe(res);
+      }
+  })
+  .listen(3000);
--- routing.js
+++ routing.js
@@ -1,60 +1,61 @@
-const fs=require('fs');
-const url=require('url');
+// const fs=require('fs');
+// // const url=require('url');
 
+// module.exports={
+//   renderHTML:function(path,response)
+// {
 
-function  renderHTML(path,response)
-{
+//     fs.readFile(path,null,(error,data)=>{
+// if(error){
+//     response.writeHead(404);
+//     response.write("File not found");
 
-    fs.readFile(path,null,(error,data)=>{
-if(error){
-    response.writeHead(404);
-    response.write("File not found");
+// }
+// else {
 
-}
-else {
+//     response.write(data);
+// }
+// response.end();
 
-    response.write(data);
-}
-response.end();
+// }
+// );
 
-}
-);
+// }
+// }
 
-}
-
-module.exports={
-    handleRequest:function(request,response){
-        response.writeHead(200,{
-            'Content-Type':'text/html'
-        });
-var path=url.parse(request.url).pathname;
+// module.exports={
+//     handleRequest:function(request,response){
+      
     
-switch(path){
-    case '/':
-        renderHTML('./views/home.html',response);
-        break;
-
-        case '/index':
-            renderHTML('./views/index.html',response);
-            break;
+     
+// var path=url.parse(request.url).pathname;
     
-    case '/login':
-        renderHTML('../login.html',response);
-        break;    
+// switch(path){
+//     case '/':
+//         renderHTML('./views/home.html',response);
+//         break;
+
+//         case '/index':
+//             renderHTML('./views/index.html',response);
+//             break;
+    
+//     case '/login':
+//         renderHTML('../login.html',response);
+//         break;    
         
-    case '/about':
-        renderHTML('./views/about.html',response);
-        break;    
+//     case '/about':
+//         renderHTML('./views/about.html',response);
+//         break;    
 
-        case '/recent':
-            renderHTML('./views/recent.html',response);
-            break;    
+//         case '/recent':
+//             renderHTML('./views/recent.html',response);
+//             break;    
 
-            default:
-                response.writeHead(404);
-                response.write("Route not defined");
-                response.end();
-}
-}
+//             default:
+//                 response.writeHead(404);
+//                 response.write("Route not defined");
+//                 response.end();
+// }
+// }
 
-}
+// }
